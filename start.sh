7461304963c770921bb0aa73cb0ab2cd0b3f7aeb
Xueshan Feng <xueshan.feng@gmail.com>
2016-10-19 19:19:13 -0700

    add openssl ca-certificates packages to use https for wget.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apk add --no-cache wget
+apk update && apk add --no-cache wget openssl ca-certificates
 VERSION=${VERSION:-v1.4.3}
 COMPONENTS=${COMPONENTS:-"kube-apiserver kube-controller-manager kube-scheduler kubectl"}
 mkdir -p /shared /tmp/download
@@ -7,7 +7,7 @@ cd /var/tmp
 for i in $COMPONENTS
 do
     echo "Install $VERSION/bin/linux/amd64/$i"
-    wget -q --no-check-certificate https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/$i
+    wget -q https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/$i
     chmod 755 $i
     cp $i /shared
 done
